07713913db3610cd934c1aa95e05e1e99437834e
Mukul Jain <mukuljain422@gmail.com>
2021-12-22 19:37:24 +0530

    validate header along with test cases

--- tests/test_indian_state_code.py
+++ tests/test_indian_state_code.py
@@ -4,7 +4,6 @@ from indian_census_exception import IndianCensusException
 
 
 class TestCase:
-
     file_path = "../indian_state_code_data.csv"
     wrong_file_extension = "../indian_state_code_data.txt"
 
@@ -53,4 +52,21 @@ class TestCase:
         expected = "Error occurred in delimiter matching"
         with pytest.raises(IndianCensusException) as exception:
             state_code.delimiter_validation(self.wrong_file_extension)
+        assert exception.value.message == expected
+
+    def test_match_header(self, state_code):
+        """
+            desc: test the method to check header
+        """
+        expected = True
+        result = state_code.validate_header(self.file_path)
+        assert result == expected
+
+    def test_not_match_header(self, state_code):
+        """
+            desc: test the method to raise exception while checking headers
+        """
+        expected = "Heading is corrupted"
+        with pytest.raises(IndianCensusException) as exception:
+            state_code.validate_header(self.wrong_file_extension)
         assert exception.value.message == expected
