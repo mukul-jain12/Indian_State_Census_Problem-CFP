252a48bc610fc98505df66b22d7f664b34948eee
Mukul Jain <mukuljain422@gmail.com>
2021-12-22 18:43:54 +0530

    count number of records along with test case

--- indian_state_code.py
+++ indian_state_code.py
@@ -8,16 +8,26 @@ import pandas as pd
 
 class IndianStateCode:
 
-    def load_indian_census_data(self, csv_file):
+    def load_indian_state_code_data(self, csv_file):
         """
             desc: load data of file
             return: dataframe
         """
-        dataframe = pd.read_csv(csv_file, encoding='unicode_escape')
+        dataframe = pd.read_csv(csv_file)
         return dataframe
 
+    def count_number_of_records(self, csv_file):
+        """
+            desc: count total number of rows
+            return: count_records
+        """
+        dataframe = self.load_indian_state_code_data(csv_file)
+        count_records = dataframe.shape[0]
+        return count_records
 
-csv_data = IndianStateCode()
-file_name = "indian_state_code_data.csv"
 
-print(csv_data.load_indian_census_data(file_name))
+if __name__ == "__main__":
+    csv_data = IndianStateCode()
+    file_name = "indian_state_code_data.csv"
+
+    print(csv_data.count_number_of_records(file_name))
